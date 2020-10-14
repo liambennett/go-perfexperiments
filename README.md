d7057e3a5d8c413de3cd70c306c3c4fd2ba39f8b
Liam Bennett <liampbennett@gmail.com>
2020-10-14 19:37:47 +0100

    tidying up

---
+++ README.md
@@ -0,0 +1,9 @@
+go version go1.13.4 darwin/amd64
+
+goos: darwin
+goarch: amd64
+BenchmarkLoop1-8   	 1654904	       739 ns/op	      72 B/op	       9 allocs/op
+BenchmarkLoop2-8   	 1692212	       709 ns/op	      64 B/op	       8 allocs/op
+BenchmarkLoop5-8   	 1793840	       691 ns/op	      40 B/op	       5 allocs/op
+PASS
+ok  	_/Users/liambennett/code/go-perfexperiments	5.857s
--- loopunrolling_test.go
+++ loopunrolling_test.go
@@ -3,21 +3,18 @@ package main
 import "testing"
 
 func BenchmarkLoop1(b *testing.B) {
-	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
 		Loop1(10000)
 	}
 }
 
 func BenchmarkLoop2(b *testing.B) {
-	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
 		Loop2(10000)
 	}
 }
 
 func BenchmarkLoop5(b *testing.B) {
-	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
 		Loop5(10000)
 	}
